7f9d9937a119af129d04d54293aad448d235f7f5
chadfreer <chadwfreer@gmail.com>
2021-09-14 09:12:05 -0400

    Adding in the boosting. (Luca) and Christos' method 	modified:   runner.py 	modified:   workflows/SUEP_coffea.py

--- workflows/SUEP_coffea.py
+++ workflows/SUEP_coffea.py
@@ -20,93 +20,83 @@ class SUEP_cluster(processor.ProcessorABC):
 
         #Set up for the histograms
         self._accumulator = processor.dict_accumulator({
-            "uncleaned_tracks": hist.Hist(
-                "Events",
+            "uncleaned_tracks": hist.Hist("Events",
                 hist.Bin("Uncleaned_Ntracks", "Uncleaned NTracks", 10000, 0, 10000)
             ),
-            "nCleaned_Cands": hist.Hist(
-                "Events",
+            "nCleaned_Cands": hist.Hist("Events",
                 hist.Bin("nCleaned_Cands", "NTracks", 200, 0, 200)
             ),
-            "pT" : hist.Hist(
-                "Events", 
-                hist.Bin("pT", "$pT$ [GeV]", 200, 0, 200)
-            ),
-            "ngood_fastjets" : hist.Hist(
-                "Events",
+            "ngood_fastjets" : hist.Hist("Events",
                 hist.Bin("ngood_fastjets", "# Fastjets", 15, 0, 15)
             ),
-            "SUEP_mult_nconst" : hist.Hist(
-                "Events",
+            "SUEP_mult_nconst" : hist.Hist("Events",
                 hist.Bin("SUEP_mult_nconst", "# Tracks", 250, 0, 250)
             ),
-            "SUEP_mult_pt" : hist.Hist(
-                "Events",
+            "SUEP_mult_pt" : hist.Hist("Events",
                 hist.Bin("SUEP_mult_pt", "pT", 100, 0, 2000)
             ),
-            "SUEP_mult_eta" : hist.Hist(
-                "Events",
+            "SUEP_mult_eta" : hist.Hist("Events",
                 hist.Bin("SUEP_mult_eta", "eta", 100, -5, 5)
             ),
-            "SUEP_mult_phi" : hist.Hist(
-                "Events",
+            "SUEP_mult_phi" : hist.Hist("Events",
                 hist.Bin("SUEP_mult_phi", "phi", 100, 0, 6.5)
             ),
-            "SUEP_mult_mass" : hist.Hist(
-                "Events",
+            "SUEP_mult_mass" : hist.Hist("Events",
                 hist.Bin("SUEP_mult_mass", "mass", 150, 0, 4000)
             ),
-            "SUEP_mult_spher" : hist.Hist(
-                "Events",
+            "SUEP_mult_spher" : hist.Hist("Events",
                 hist.Bin("SUEP_mult_spher", "Sphericity", 100, 0, 1)
             ),
-            "SUEP_mult_aplan" : hist.Hist(
-                "Events",
+            "SUEP_mult_aplan" : hist.Hist("Events",
                 hist.Bin("SUEP_mult_aplan", "Aplanarity", 100, 0, 1)
             ),
-            "SUEP_mult_FW2M" : hist.Hist(
-                "Events",
+            "SUEP_mult_FW2M" : hist.Hist("Events",
                 hist.Bin("SUEP_mult_FW2M", "2nd Fox Wolfram Moment", 100, 0, 1)
             ),
-            "SUEP_mult_D" : hist.Hist(
-                "Events",
+            "SUEP_mult_D" : hist.Hist("Events",
                 hist.Bin("SUEP_mult_D", "D", 100, 0, 1)
             ),
-            "SUEP_pt_nconst" : hist.Hist(
-                "Events",
+            "SUEP_pt_nconst" : hist.Hist("Events",
                 hist.Bin("SUEP_pt_nconst", "# Tracks", 250, 0, 250)
             ),
-            "SUEP_pt_pt" : hist.Hist(
-                "Events",
+            "SUEP_pt_pt" : hist.Hist("Events",
                 hist.Bin("SUEP_pt_pt", "pT", 100, 0, 2000)
             ),
-            "SUEP_pt_eta" : hist.Hist(
-                "Events",
+            "SUEP_pt_eta" : hist.Hist("Events",
                 hist.Bin("SUEP_pt_eta", "eta", 100, -5, 5)
             ),
-            "SUEP_pt_phi" : hist.Hist(
-                "Events",
+            "SUEP_pt_phi" : hist.Hist("Events",
                 hist.Bin("SUEP_pt_phi", "phi", 100, 0, 6.5)
             ),
-            "SUEP_pt_mass" : hist.Hist(
-                "Events",
+            "SUEP_pt_mass" : hist.Hist("Events",
                 hist.Bin("SUEP_pt_mass", "mass", 150, 0, 4000)
             ),
-            "SUEP_pt_spher" : hist.Hist(
-                "Events",
+            "SUEP_pt_spher" : hist.Hist("Events",
                 hist.Bin("SUEP_pt_spher", "Sphericity", 100, 0, 1)
             ),  
-            "SUEP_pt_aplan" : hist.Hist(
-                "Events",
+            "SUEP_pt_aplan" : hist.Hist("Events",
                 hist.Bin("SUEP_pt_aplan", "Aplanarity", 100, 0, 1)
             ),  
-            "SUEP_pt_FW2M" : hist.Hist(
-                "Events", 
+            "SUEP_pt_FW2M" : hist.Hist("Events", 
                 hist.Bin("SUEP_pt_FW2M", "2nd Fox Wolfram Moment", 100, 0, 1)
             ),  
-            "SUEP_pt_D" : hist.Hist(
-                "Events", 
+            "SUEP_pt_D" : hist.Hist("Events", 
                 hist.Bin("SUEP_pt_D", "D", 100, 0, 1)
+            ),
+            "SUEP_ch_nconst" : hist.Hist("Events",
+                hist.Bin("SUEP_ch_nconst", "# Tracks", 250, 0, 250)
+            ),
+            "SUEP_ch_spher" : hist.Hist("Events",
+                hist.Bin("SUEP_ch_spher", "Sphericity", 100, 0, 1)
+            ),
+            "SUEP_ch_aplan" : hist.Hist("Events",
+                hist.Bin("SUEP_ch_aplan", "Aplanarity", 100, 0, 1)
+            ),
+            "SUEP_ch_FW2M" : hist.Hist("Events",
+                hist.Bin("SUEP_ch_FW2M", "2nd Fox Wolfram Moment", 100, 0, 1)
+            ),
+            "SUEP_ch_D" : hist.Hist("Events",
+                hist.Bin("SUEP_ch_D", "D", 100, 0, 1)
             )
         })
 
@@ -139,6 +129,7 @@ class SUEP_cluster(processor.ProcessorABC):
     def process(self, events):
         output = self.accumulator.identity()
         dataset = events.metadata['dataset']
+
         #Prepare the clean track collection
         Cands = ak.zip({
             "pt": events.PFCands.trkPt,
@@ -149,101 +140,93 @@ class SUEP_cluster(processor.ProcessorABC):
         cut = (events.PFCands.fromPV > 1) & (events.PFCands.trkPt >= 1) & (events.PFCands.trkEta <= 2.5)
         Cleaned_cands = Cands[cut]
         Cleaned_cands = ak.packed(Cleaned_cands)
+        output["uncleaned_tracks"].fill(Uncleaned_Ntracks = ak.num(Cands))
+        output["nCleaned_Cands"].fill(nCleaned_Cands = ak.num(Cleaned_cands))
 
         #The jet clustering part
         jetdef = fastjet.JetDefinition(fastjet.antikt_algorithm, 1.5)
         cluster = fastjet.ClusterSequence(Cleaned_cands, jetdef) 
-        ak_inclusive_jets = ak.with_name(cluster.inclusive_jets(min_pt=3),"Momentum4D")
-        ak_inclusive_cluster = ak.with_name(cluster.constituents(min_pt=3),"Momentum4D")
+        ak_inclusive_jets = ak.with_name(cluster.inclusive_jets(min_pt=150),"Momentum4D")
+        ak_inclusive_cluster = ak.with_name(cluster.constituents(min_pt=150),"Momentum4D")
+        output["ngood_fastjets"].fill(ngood_fastjets = ak.num(ak_inclusive_jets))
 
         #remove events without a cluster
-        ak_inclusive_cluster = ak_inclusive_cluster[ak.num(ak_inclusive_jets, axis=1)>0]
-        ak_inclusive_jets = ak_inclusive_jets[ak.num(ak_inclusive_jets, axis=1)>0]
+        ak_inclusive_cluster = ak_inclusive_cluster[ak.num(ak_inclusive_jets, axis=1)>1]
+        ak_inclusive_jets = ak_inclusive_jets[ak.num(ak_inclusive_jets, axis=1)>1]
         
         #SUEP_mult
         chonkocity = ak.num(ak_inclusive_cluster, axis=2)
         chonkiest_jet = ak.argsort(chonkocity, axis=1, ascending=True, stable=True)[:, ::-1] 
         thicc_jets = ak_inclusive_jets[chonkiest_jet]
-                
+        chonkiest_cands = ak_inclusive_cluster[chonkiest_jet][:,0]
+        output["SUEP_mult_nconst"].fill(SUEP_mult_nconst = ak.max(ak.num(ak_inclusive_cluster, axis=2),axis=1))
+        output["SUEP_mult_pt"].fill(SUEP_mult_pt = thicc_jets[:,0].pt)
+        output["SUEP_mult_eta"].fill(SUEP_mult_eta = thicc_jets[:,0].eta)
+        output["SUEP_mult_phi"].fill(SUEP_mult_phi = thicc_jets[:,0].phi)
+        output["SUEP_mult_mass"].fill(SUEP_mult_mass = thicc_jets[:,0].mass)
+
+        #SUEP_mult boosting and sphericity
+        boost_mult = ak.zip({
+            "px": thicc_jets[:,0].px*-1,
+            "py": thicc_jets[:,0].py*-1,
+            "pz": thicc_jets[:,0].pz*-1,
+            "mass": thicc_jets[:,0].mass
+        }, with_name="Momentum4D")
+        chonkiest_cands = chonkiest_cands.boost_p4(-boost_mult)
+        mult_eigs = self.sphericity(chonkiest_cands,2.0)        
+        output["SUEP_mult_spher"].fill(SUEP_mult_spher = 1.5 * (mult_eigs[:,1]+mult_eigs[:,0]))
+        output["SUEP_mult_aplan"].fill(SUEP_mult_aplan = 1.5 * mult_eigs[:,0])
+        output["SUEP_mult_FW2M"].fill(SUEP_mult_FW2M = 1.0 - 3.0 * (mult_eigs[:,2]*mult_eigs[:,1] + mult_eigs[:,0]*mult_eigs[:,2] + mult_eigs[:,1]*mult_eigs[:,0]))
+        output["SUEP_mult_D"].fill(SUEP_mult_D = 27.0 * mult_eigs[:,2]*mult_eigs[:,1]*mult_eigs[:,0])       
+
         #SUEP_pt
         highpt_jet = ak.argsort(ak_inclusive_jets.pt, axis=1, ascending=False, stable=True)
         SUEP_pt = ak_inclusive_jets[highpt_jet]
         SUEP_pt_constituent = chonkocity[highpt_jet]
-
-        #Sphericity tensor
-        chonkiest_cands = ak_inclusive_cluster[chonkiest_jet][:,0]
-        mult_eigs = self.sphericity(chonkiest_cands,2.0)
-
         highpt_cands = ak_inclusive_cluster[highpt_jet][:,0]
-        pt_eigs = self.sphericity(highpt_cands,2.0)
+        output["SUEP_pt_nconst"].fill(SUEP_pt_nconst = SUEP_pt_constituent[:,0])
+        output["SUEP_pt_pt"].fill(SUEP_pt_pt = SUEP_pt[:,0].pt)
+        output["SUEP_pt_eta"].fill(SUEP_pt_eta = SUEP_pt[:,0].eta)
+        output["SUEP_pt_phi"].fill(SUEP_pt_phi = SUEP_pt[:,0].phi)
+        output["SUEP_pt_mass"].fill(SUEP_pt_mass = SUEP_pt[:,0].mass)
 
-        #Now we will fill
-        output["uncleaned_tracks"].fill(
-            Uncleaned_Ntracks = ak.num(Cands),
-        ) 
-        output["nCleaned_Cands"].fill(
-            nCleaned_Cands = ak.num(Cleaned_cands),
-        )
-        output["pT"].fill(
-            pT = (Cleaned_cands.px[0]),
-        )
-        output["ngood_fastjets"].fill(
-            ngood_fastjets = ak.num(ak_inclusive_jets),
-        )
-        output["SUEP_mult_nconst"].fill(
-            SUEP_mult_nconst = ak.max(ak.num(ak_inclusive_cluster, axis=2),axis=1)
-        )
-        output["SUEP_mult_pt"].fill(
-            SUEP_mult_pt = thicc_jets[:,0].pt
-        )
-        output["SUEP_mult_eta"].fill(
-            SUEP_mult_eta = thicc_jets[:,0].eta
-        )
-        output["SUEP_mult_phi"].fill(
-            SUEP_mult_phi = thicc_jets[:,0].phi
-        )
-        output["SUEP_mult_mass"].fill(
-            SUEP_mult_mass = thicc_jets[:,0].mass
-        )
-        output["SUEP_mult_spher"].fill(
-            SUEP_mult_spher = 1.5 * (mult_eigs[:,1]+mult_eigs[:,0])
-        )
-        output["SUEP_mult_aplan"].fill(
-            SUEP_mult_aplan = 1.5 * mult_eigs[:,0]
-        )
-        output["SUEP_mult_FW2M"].fill(
-            SUEP_mult_FW2M = 1.0 - 3.0 * (mult_eigs[:,2]*mult_eigs[:,1] + mult_eigs[:,0]*mult_eigs[:,2] + mult_eigs[:,1]*mult_eigs[:,0])
-        )
-        output["SUEP_mult_D"].fill(
-            SUEP_mult_D = 27.0 * mult_eigs[:,2]*mult_eigs[:,1]*mult_eigs[:,0]
-        )
-        output["SUEP_pt_nconst"].fill(
-            SUEP_pt_nconst = SUEP_pt_constituent[:,0]
-        )
-        output["SUEP_pt_pt"].fill(
-            SUEP_pt_pt = SUEP_pt[:,0].pt
-        )
-        output["SUEP_pt_eta"].fill(
-            SUEP_pt_eta = SUEP_pt[:,0].eta
-        )
-        output["SUEP_pt_phi"].fill(
-            SUEP_pt_phi = SUEP_pt[:,0].phi
-        )
-        output["SUEP_pt_mass"].fill(
-            SUEP_pt_mass = SUEP_pt[:,0].mass
-        ) 
-        output["SUEP_pt_spher"].fill(
-            SUEP_pt_spher = 1.5 * (pt_eigs[:,1]+pt_eigs[:,0])
-        )
-        output["SUEP_pt_aplan"].fill(
-            SUEP_pt_aplan = 1.5 * pt_eigs[:,0]
-        )
-        output["SUEP_pt_FW2M"].fill(
-            SUEP_pt_FW2M = 1.0 - 3.0 * (pt_eigs[:,2]*pt_eigs[:,1] + pt_eigs[:,2]*pt_eigs[:,0] + pt_eigs[:,1]*pt_eigs[:,0])
-        )
-        output["SUEP_pt_D"].fill(
-            SUEP_pt_D = 27.0 * pt_eigs[:,2]*pt_eigs[:,1]*pt_eigs[:,0]
-        )
+        #SUEP_pt boosting and sphericity
+        boost_pt = ak.zip({
+            "px": SUEP_pt[:,0].px*-1,
+            "py": SUEP_pt[:,0].py*-1,
+            "pz": SUEP_pt[:,0].pz*-1,
+            "mass": SUEP_pt[:,0].mass
+        }, with_name="Momentum4D")
+        highpt_cands = highpt_cands.boost_p4(boost_pt)
+        pt_eigs = self.sphericity(highpt_cands,2.0)
+        output["SUEP_pt_spher"].fill(SUEP_pt_spher = 1.5 * (pt_eigs[:,1]+pt_eigs[:,0]))
+        output["SUEP_pt_aplan"].fill(SUEP_pt_aplan = 1.5 * pt_eigs[:,0])
+        output["SUEP_pt_FW2M"].fill(SUEP_pt_FW2M = 1.0 - 3.0 * (pt_eigs[:,2]*pt_eigs[:,1] + pt_eigs[:,2]*pt_eigs[:,0] + pt_eigs[:,1]*pt_eigs[:,0]))
+        output["SUEP_pt_D"].fill(SUEP_pt_D = 27.0 * pt_eigs[:,2]*pt_eigs[:,1]*pt_eigs[:,0])
+
+        #Christos Method for ISR removal
+        SUEP_cand = SUEP_pt[:,0]
+        ISR_cand = SUEP_pt[:,1]
+        #if SUEP_pt_constituent[:,0] < SUEP_pt_constituent[:,1]:
+        boost_ch = ak.zip({
+            "px": SUEP_cand.px*-1,
+            "py": SUEP_cand.py*-1,
+            "pz": SUEP_cand.pz*-1,
+            "mass": SUEP_cand.mass
+        }, with_name="Momentum4D")
+        ISR_cand = ISR_cand.boost_p4(boost_ch)
+        Christos_cands = Cleaned_cands[ak.num(ak_inclusive_jets)>1]
+        Christos_cands = Christos_cands.boost_p4(boost_ch)
+        Christos_cands = Christos_cands[abs(Christos_cands.deltaphi(ISR_cand)) > 1.6]
+        print(ak.num(Christos_cands))
+        Christos_cands = Christos_cands[ak.num(Christos_cands)>0]
+        ch_eigs = self.sphericity(Christos_cands,2.0)
+        output["SUEP_ch_nconst"].fill(SUEP_ch_nconst = ak.num(Christos_cands))
+        output["SUEP_ch_spher"].fill(SUEP_ch_spher = 1.5 * (ch_eigs[:,1]+ch_eigs[:,0]))
+        output["SUEP_ch_aplan"].fill(SUEP_ch_aplan = 1.5 * ch_eigs[:,0])
+        output["SUEP_ch_FW2M"].fill(SUEP_ch_FW2M = 1.0 - 3.0 * (ch_eigs[:,2]*ch_eigs[:,1] + ch_eigs[:,2]*ch_eigs[:,0] + ch_eigs[:,1]*ch_eigs[:,0]))
+        output["SUEP_ch_D"].fill(SUEP_ch_D = 27.0 * ch_eigs[:,2]*ch_eigs[:,1]*ch_eigs[:,0])
+
         return output
 
     def postprocess(self, accumulator):
