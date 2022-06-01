30677385ac5c983ec3a3e4347ff7f463561e45f5
cericeci <cericeci@cern.ch>
2022-06-01 18:34:02 +0200

    OF based selection for ttbar backgrounds

--- workflows/SUEP_coffea_ZH_simple.py
+++ workflows/SUEP_coffea_ZH_simple.py
@@ -18,9 +18,10 @@ from typing import List, Optional
 vector.register_awkward()
 
 class SUEP_cluster(processor.ProcessorABC):
-    def __init__(self, isMC: int, era: int, sample: str,  do_syst: bool, syst_var: str, weight_syst: bool, flag: bool, output_location: Optional[str]) -> None:
+    def __init__(self, isMC: int, era: int, sample: str,  do_syst: bool, syst_var: str, weight_syst: bool, flag: bool, output_location: Optional[str], doOF: Optional[bool]) -> None:
         self._flag = flag
         self.output_location = output_location
+        self.doOF = doOF
         self.do_syst = do_syst
         self.gensumweight = 1.0
         self.era = era
@@ -224,15 +225,29 @@ class SUEP_cluster(processor.ProcessorABC):
         #  First, number of muons (axis=1 means column. Each row is an event.) in an event is 2.
         #  Second, pt of the muons is greater than 25.
         #  Third, Sum of charge of muons should be 0. (because it originates from Z)
-        cutHasTwoMuons = (ak.num(selMuons, axis=1)==2) & (ak.max(selMuons.pt, axis=1, mask_identity=False) >= 25) & (ak.sum(selMuons.charge,axis=1) == 0)
-        cutHasTwoElecs = (ak.num(selElectrons, axis=1)==2) & (ak.max(selElectrons.pt, axis=1, mask_identity=False) >= 25) & (ak.sum(selElectrons.charge,axis=1) == 0)
-        cutTwoLeps     = ((ak.num(selElectrons, axis=1)+ak.num(selMuons, axis=1)) < 4)
-        cutHasTwoLeps  = ((cutHasTwoMuons) | (cutHasTwoElecs)) & cutTwoLeps
+        if self.doOF:
+          # Only for the OF sideband for tt/WW/Fakes estimation
+          templeps = ak.concatenate([selMuons,selElectrons], axis=1)
+          cutHasOFLeps =  (ak.num(templeps, axis=1)==2) & (ak.max(templeps.pt, axis=1, mask_identity=False) >= 25) & (ak.sum(templeps.charge,axis=1) == 0) 
+          events = events[cutHasOFLeps]
+          selElectrons = selElectrons[cutHasOFLeps]
+          selMuons = selMuons[cutHasOFLeps]
+          cutOneAndOne = (ak.num(selElectrons) == 1) & (ak.num(selMuons) == 1)
+          events = events[cutOneAndOne]
+          selElectrons = selElectrons[cutOneAndOne]
+          selMuons     = selMuons[cutOneAndOne] 
 
-        ### Cut the events, also return the selected leptons for operation down the line
-
-        events = events[ cutHasTwoLeps]
-        return events, selElectrons[cutHasTwoLeps], selMuons[cutHasTwoLeps], [coll[cutHasTwoLeps] for coll in extraColls]
+        else:
+          cutHasTwoMuons = (ak.num(selMuons, axis=1)==2) & (ak.max(selMuons.pt, axis=1, mask_identity=False) >= 25) & (ak.sum(selMuons.charge,axis=1) == 0)
+          cutHasTwoElecs = (ak.num(selElectrons, axis=1)==2) & (ak.max(selElectrons.pt, axis=1, mask_identity=False) >= 25) & (ak.sum(selElectrons.charge,axis=1) == 0)
+          cutTwoLeps     = ((ak.num(selElectrons, axis=1)+ak.num(selMuons, axis=1)) < 4)
+          cutHasTwoLeps  = ((cutHasTwoMuons) | (cutHasTwoElecs)) & cutTwoLeps
+          ### Cut the events, also return the selected leptons for operation down the line
+          events = events[cutHasTwoLeps]
+          selElectrons = selElectrons[cutHasTwoLeps]
+          selMuons = selMuons[cutHasTwoLeps]
+          
+        return events, selElectrons, selMuons #, [coll[cutHasTwoLeps] for coll in extraColls]
 
     def selectByJets(self, events, leptons = [],  extraColls = []):
         # These are just standard jets, as available in the nanoAOD
@@ -359,7 +374,8 @@ class SUEP_cluster(processor.ProcessorABC):
 
         # Data dependant stuff
         dataset = events.metadata['dataset']
-        if self.isMC:      self.gensumweight = ak.sum(events.genWeight)
+        if self.isMC: self.gensumweight = ak.sum(events.genWeight)
+
         if not(self.isMC): doGen = False
 
         # ------------------------------------------------------------------------------------
