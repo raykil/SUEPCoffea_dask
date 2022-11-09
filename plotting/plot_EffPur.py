89a6a6adebffcb7d9f04c2183faeee90dbd33aa0
Jungmin Kil <jungmin.kil@cern.ch>
2022-11-09 06:48:58 +0100

    with pt and pz cuts. In progress

--- plotting/plot_EffPur.py
+++ plotting/plot_EffPur.py
@@ -15,17 +15,43 @@ import matplotlib.pyplot as plt
 # clusterEfficiency = nSUEPinCluster / nSUEPtracks
 # clusterPurity = nSUEPinCluster / nInCluster
 
-ZH = [pd.HDFStore("../outputSimTracks/"+f, 'r') for f in os.listdir("../outputSimTracks/")]
+ZH = [pd.HDFStore("../outputSimTracksTest/"+f, 'r') for f in os.listdir("../outputSimTracksTest/")]
 output = "/eos/user/j/jkil/www/EfficiencyAndPurity"
 
-plotStripEff = False
-plotStripPur = False
-plotClusterEff = False
-plotClusterPur = False
 bothEff = True
 bothPur = True
 
-###### CLUSTER ######
+HighPtOnly = False # Make it True to enable high pt cut. Careful! Make sure to disable LowPtOnly
+LowPtOnly = False  # Make it True to enable low pt cut. Careful! Make sure to disable HighPtOnly
+
+HighPzOnly = False
+LowPzOnly = False
+
+###### GEN VARS ######
+
+Hpt = [0]*len(ZH)
+Hpz = [0]*len(ZH)
+ptCutVal = 50.0
+pzCutVal = 0.0
+
+for i in range(len(ZH)):
+    genHpt = ZH[i]["onecluster"]["genHpt"]
+    genHpz = ZH[i]["onecluster"]["genHpz"]
+    Hpt[i] = pd.Series.tolist(genHpt)
+    Hpz[i] = pd.Series.tolist(genHpz)
+
+Hpt = sum(Hpt,[])
+print(len(Hpt))
+Hpz = sum(Hpz,[])
+
+highHptCut = (np.array(Hpt)[:] > ptCutVal) # Only the ones with high pt survives
+lowHptCut = (np.array(Hpt)[:] < ptCutVal)
+
+highHpzCut = (np.array(Hpz)[:] > pzCutVal)
+lowHpzCut = (np.array(Hpz)[:] < pzCutVal)
+
+
+###### CLUSTER VARIABLES ######
 
 clusterEff = [0]*len(ZH)
 clusterPur = [0]*len(ZH)
@@ -33,40 +59,42 @@ clusterPur = [0]*len(ZH)
 for i in range(len(ZH)):
     nSUEPtracks = ZH[i]["onecluster"]["nSUEPtracks"]
     nInCluster = ZH[i]["onecluster"]["nInCluster"] 
-    nSUEPinCluster = ZH[i]["onecluster"]["nSUEPinCluster"] 
+    nSUEPinCluster = ZH[i]["onecluster"]["nSUEPinCluster"]
 
     clusterEff[i] = pd.Series.tolist(nSUEPinCluster/nSUEPtracks)
     clusterPur[i] = pd.Series.tolist(nSUEPinCluster/nInCluster)
 
-clusterEff = sum(clusterEff,[])
-clusterPur = sum(clusterPur,[])
+clusterEff = np.array(sum(clusterEff,[]))
+clusterPur = np.array(sum(clusterPur,[]))
 
-clusterPurCut = (np.array(clusterPur)[:] <= 1)
-clusterPur = np.array(clusterPur)[clusterPurCut]
+clusterEffCut = (clusterEff[:] <= 1)
+clusterEff = clusterEff[clusterEffCut]
+
+# Applying pt cut
+if HighPtOnly & (LowPtOnly == False):
+    clusterEff = clusterEff[highHptCut]
+    clusterPur = clusterPur[highHptCut]
+elif LowPtOnly & (HighPtOnly == False):
+    clusterEff = clusterEff[lowHptCut]
+    clusterPur = clusterPur[lowHptCut]
+elif HighPtOnly & LowPtOnly: print("Make sure you are choosing either high/low pt! Now drawing with no cuts.")
+
+# Applying pz cut
+if HighPzOnly & (LowPzOnly == False):
+    clusterEff = clusterEff[highHpzCut]
+    clusterPur = clusterPur[highHpzCut]
+elif LowPzOnly & (HighPzOnly == False):
+    clusterEff = clusterEff[lowHpzCut]
+    clusterPur = clusterPur[lowHpzCut]
+elif HighPzOnly & LowPzOnly: print("Make sure you are choosing either high/low pz! Now drawing with no cuts.")
 
 
-if plotClusterEff:
-    plt.figure()
-    plt.hist(clusterEff)
-    plt.title("Efficiency of Cluster")
-    plt.xlabel("Efficiency of Cluster")
-    plt.ylabel("Counts")
-    plt.savefig("{}/clusterEff.png".format(output))
-
-if plotClusterPur:
-    plt.figure()
-    plt.hist(clusterPur)
-    plt.title("Purity of Cluster")
-    plt.xlabel("Purity of Cluster")
-    plt.ylabel("Counts")
-    plt.savefig("{}/clusterPur.png".format(output))
-
-###### STRIPS ######
+###### STRIP VARIABLES ######
 
 #EtaWidths = ["0.1","0.2","0.3","0.4","0.5","0.6","0.7","0.8","0.9","1.0"]
-#EtaWidths = ["0.1","0.2","0.3","0.4","0.5"]
+EtaWidths = ["0.2","0.4","0.8","1.0","1.2","1.4","1.6","1.8","2.0"]
+#EtaWidths = ["1.2","1.4","1.6","1.8","2.0"]
 #EtaWidths = ["0.6","0.7","0.8","0.9","1.0"]
-EtaWidths = ["1.0"]
 
 for etaWidth in EtaWidths:
     stripEff = [0]*len(ZH)
@@ -83,27 +111,32 @@ for etaWidth in EtaWidths:
     stripEff = sum(stripEff,[])
     stripPur = sum(stripPur,[])
 
-    if plotStripEff:
-        plt.figure()
-        plt.hist(stripEff)
-        plt.title("Efficiency of Strip{}".format(etaWidth))
-        plt.xlabel("Efficiency of Strip{}".format(etaWidth))
-        plt.ylabel("Counts")
-        plt.savefig("{}/strip{}Eff.png".format(output,etaWidth))
+    if HighPtOnly & (LowPtOnly == False):
+        stripEff = stripEff[highHptCut]
+        stripPur = stripPur[highHptCut]
+    elif LowPtOnly & (HighPtOnly == False):
+        stripEff = stripEff[lowHptCut]
+        stripPur = stripPur[lowHptCut]
+    elif HighPtOnly & LowPtOnly: print("Make sure you are choosing either high/low pt! Now drawing with no cuts.")
 
-    if plotStripPur:
-        plt.figure()
-        plt.hist(stripPur)
-        plt.title("Purity of Strip{}".format(etaWidth))
-        plt.xlabel("Purity of Strip{}".format(etaWidth))
-        plt.ylabel("Counts")
-        plt.savefig("{}/strip{}Pur.png".format(output,etaWidth))
+    if HighPzOnly & (LowPzOnly == False):
+        stripEff = stripEff[highHpzCut]
+        stripPur = stripPur[highHpzCut]
+    elif LowPzOnly & (HighPzOnly == False):
+        stripEff = stripEff[lowHpzCut]
+        stripPur = stripPur[lowHpzCut]
+    elif HighPzOnly & LowPzOnly: print("Make sure you are choosing either high/low pz! Now drawing with no cuts.")
 
     if bothEff:
         plt.figure()
         plt.hist(clusterEff, alpha=0.5, color="tab:blue")
         plt.hist(stripEff, alpha=0.5, color="tab:green")
         plt.title("Efficiency of Cluster and Strip{}".format(etaWidth))
+        if HighPtOnly: plt.text(.75,.75,"Hpt > {}".format(ptCutVal))
+        #if LowPtOnly:  plt.text(x,y,"Hpt > {}".format(ptCutVal))
+        #if HighPzOnly: plt.text(x,y,"Hpt > {}".format(ptCutVal))
+        #if LowPzOnly:  plt.text(x,y,"Hpt > {}".format(ptCutVal))
+
         plt.xlabel("Efficiency")
         plt.ylabel("Counts")
         plt.legend(["cluster Efficiency", "Strip{} Efficiency".format(etaWidth)])
